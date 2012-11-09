f55bebc9202dc3030b967458bebf17d43c3c8c3e
Jeffrey Drake <drakej@uwindsor.ca>
2012-11-09 13:57:17 -0500

    Using google font loader

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
+WebFont.load({
+    google: {
+      families: ['Germania One']
+    },
 
-DomReady.ready(function()
-{
-	  var canvas = document.getElementById('mapcanvas');
-      var context = canvas.getContext('2d');
-
-      // begin custom shape
-/*      context.beginPath();
-      context.moveTo(170, 80);
-      context.bezierCurveTo(130, 100, 130, 150, 230, 150);
-      context.bezierCurveTo(250, 180, 320, 180, 340, 150);
-      context.bezierCurveTo(420, 150, 420, 120, 390, 100);
-      context.bezierCurveTo(430, 40, 370, 30, 340, 50);
-      context.bezierCurveTo(320, 5, 250, 20, 250, 50);
-      context.bezierCurveTo(200, 5, 150, 20, 170, 80);
-
-      // complete custom shape
-      context.closePath();
-      context.lineWidth = 5;
-      context.fillStyle = '#8ED6FF';
-      context.fill();
-      context.strokeStyle = 'blue';
-      context.stroke(); */
-});
+    active: function() {
+		var canvas = document.getElementById('mapcanvas');
+		var context = canvas.getContext('2d');
+		
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+	
+		var midx = canvas.width / 2;
+		var midy = canvas.height / 2;
+		
+		context.font = "40px 'Germania One'";
+		
+		context.fillText("Ad Astra:", midx - 100, midy - 50);
+		context.fillText("Primitus", midx - 60, midy);
+    },
+  });
