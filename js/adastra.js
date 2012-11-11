daa105694e3b857da4fe485f835cc4609628f4dd
Eric Brisco <eric.brisco@gmail.com>
2012-11-11 00:41:52 -0500

    Moved main menu code to a class instead of module. Altered the bootstrap.

--- js/adastra.js
+++ js/adastra.js
@@ -7,64 +7,50 @@ http://www.phpied.com/3-ways-to-define-a-javascript-class/
 
 */
 
-var AdAstra = {
+function MainMenu() {
+  var canvas = document.getElementById('canvas');
+  canvas.width = document.width;
+  canvas.height = document.height;
+  var stage = this.__stage = new Stage(canvas);
+  stage.enableMouseOver();
+	stage.autoClear = false;
+  var width = this.__width = canvas.width;
+  var height = this.__height = canvas.height;
+  var stars = this.__stars = [];
+  this.__rates = [60, 45, 20];
+  this.__offsets = [0, 0, 0];
+  
+  var title = new Text("Ad Astra:", "40px 'Germania One'", "#FFFFFF");
+  title.x = width/2 - title.getMeasuredWidth()/2 - 60;
+  title.y = height/2 - title.getMeasuredHeight()/2 - 60;
+  stage.addChild(title);
+  
+  var subtitle = new Text("Primitus", "40px 'Germania One'", "#FFFFFF");
+  subtitle.x = width/2 - subtitle.getMeasuredWidth()/2 - 30;
+  subtitle.y = title.y + subtitle.getMeasuredHeight() * 1.1;
+  stage.addChild(subtitle);
+  
+  var starcounts = [75, 40, 30];
+  for (var i in starcounts)
+  {
+    var stage = new Stage(canvas);
+    this.__createStars(stage, starcounts[i]);
+    stars.push(stage);
+  }
+  
+  var g = new Graphics();
+  g.beginFill(Graphics.getRGB(0, 0, 255));
+  g.rect(0, 0, width/8, width/8);
+  var s = new Shape(g);
+  s.x = width/4;
+  s.y = height/4*3;
+  
+  stage.addChild(s);
+}
 
-	main: function()
-	{
-		// Get a reference to the canvas object
-		var canvas = document.getElementById('canvas');
-		canvas.width = document.width;
-		canvas.height = document.height;
-		
-		
-		AdAstra.stage = new Stage(document.getElementById("canvas"));
-		AdAstra.stage.enableMouseOver();
-		
-		AdAstra.stage.autoClear = false;
-		
-		AdAstra.width =  canvas.width;
-		AdAstra.height = canvas.height;
-		
-		var title = new Text("Ad Astra:", "40px 'Germania One'", "#FFFFFF");
-		title.x = AdAstra.width / 2 - title.getMeasuredWidth()/2 - 60;
-		title.y = AdAstra.height / 2 - title.getMeasuredHeight()/2 - 60;
-		AdAstra.stage.addChild(title);
-		
-		var subtitle = new Text("Primitus", "40px 'Germania One'", "#FFFFFF");
-		subtitle.x = AdAstra.width/2 - subtitle.getMeasuredWidth()/2 - 30;
-		subtitle.y = title.y + subtitle.getMeasuredHeight() * 1.1;
-		AdAstra.stage.addChild(subtitle);
-		
-		AdAstra.stage.update(); 
-		
-		Ticker.addListener(AdAstra.draw);
-		Ticker.setInterval(1000.0/60.0);
-		
-		AdAstra.stars = [];
+MainMenu.prototype = {
 
-		var starcounts = [75, 40, 30];
-		AdAstra.rates = [60, 45, 20];
-		AdAstra.offsets = [0, 0, 0];
-		
-		for (var i in starcounts)
-		{
-			var stage = new Stage(document.getElementById("canvas"));
-			this.createStars(stage, starcounts[i]);
-			AdAstra.stars.push(stage);
-		}
-	
-		var g = new Graphics();
-		g.beginFill(Graphics.getRGB(0, 0, 255));
-		g.rect(0, 0, AdAstra.width / 8, AdAstra.width / 8);
-		var s = new Shape(g);
-		s.x = AdAstra.width/4;
-		s.y = AdAstra.height / 4 * 3;
-		
-		AdAstra.stage.addChild(s);	
-		
-	},
-	
-	createStars: function(stage, n)
+  __createStars: function(stage, n)
 	{
 		stage.autoClear = false;
 		
@@ -73,8 +59,8 @@ var AdAstra = {
 
 		for (var a = 0; a < n; a++)
 		{
-			x = Math.floor(Math.random() * AdAstra.width / 2) * 2;
-			y = Math.floor(Math.random() * AdAstra.height / 2) * 2;
+			x = Math.floor(Math.random() * this.__width / 2) * 2;
+			y = Math.floor(Math.random() * this.__height / 2) * 2;
 
 			g.beginFill(Graphics.getRGB(255, 255, 255));
 			g.drawCircle(x, y, 1.5);
@@ -88,45 +74,45 @@ var AdAstra = {
 		
 		stage.addChild(shape1);
 		
-		stage.cache(0, 0, AdAstra.width, AdAstra.height, 1.0);
+		stage.cache(0, 0, this.__width, this.__height, 1.0);
 	},
-	
-	displayStar: function(i, dt)
+
+  __displayStar: function(i, dt)
 	{
-		var n = AdAstra.rates[i];
-		var o = AdAstra.offsets[i];
+		var n = this.__rates[i];
+		var o = this.__offsets[i];
 		
-		o += (AdAstra.width) * ((dt / 1000.0)/ n)
-		if (o > AdAstra.width)
+		o += (this.__width) * ((dt / 1000.0)/ n)
+		if (o > this.__width)
 			o = 0;
 		
 		var canvas = document.getElementById('canvas');
 		var context = canvas.getContext('2d');
-		var w = AdAstra.width;
-		var h = AdAstra.height;
+		var w = this.__width;
+		var h = this.__height;
 		
-		context.drawImage(AdAstra.stars[i].cacheCanvas, o, 0, w - o, h,
+		context.drawImage(this.__stars[i].cacheCanvas, o, 0, w - o, h,
 										 0, 0, w - o, h);
 				
 		if (o != 0)
-			context.drawImage(AdAstra.stars[i].cacheCanvas, 0, 0, o, h,
+			context.drawImage(this.__stars[i].cacheCanvas, 0, 0, o, h,
 			 								 w - o, 0, o, h);
 			 								 
-		AdAstra.offsets[i] = o;
+		this.__offsets[i] = o;
 		
 	},
 
-	
 	draw: function(dt)
 	{
-		AdAstra.stage.clear();
+		this.__stage.clear();
 		
-		for (var i = 0; i < AdAstra.stars.length; i++)
+		for (var i = 0; i < this.__stars.length; i++)
 		{
-			AdAstra.displayStar(i, dt);
+			this.__displayStar(i, dt);
 		}
 				
-		AdAstra.stage.update();
+		this.__stage.update();
 		
 	}
+  
 }
--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-WebFont.load({
+window.onload = function () {
+  WebFont.load({
     google: {
       families: ['Germania One']
     },
 
     active: function() 
     {
-    	AdAstra.main();
+    	preloadComplete();
     },
     
   });
+}
 
-
+function preloadComplete() {
+  var mainMenu = new MainMenu();
+  var services = new GFW.Standard.Services();
+  var draw = function () {
+    mainMenu.draw(services.getTime().getDelta());
+  }
+  var logic = function () {}
+  var gameLoop = new GFW.Standard.GameLoop(services, logic, draw, 1000/60, 5);
+  gameLoop.run();
+}
