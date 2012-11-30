22b41ebb0d20cf3b0de2e0cf26cd2a01ba5a6c05
rybadour <rybadour@gmail.com>
2012-11-30 16:01:23 -0500

    Updated: The Tile component now correctly makes a rectangular tile with an optional text portion and image.

--- js/main.js
+++ js/main.js
@@ -3,14 +3,18 @@ Crafty.scene("main", function ()
 	// Load the initial images
 	Crafty.load(["img/plus.png"], function ()
 	{
-		Crafty.e("2D, DOM, Image").attr({ w: 100, h: 100, x: 50, y: 50 })
-			.image("img/plus.png");
+		var newGame = Crafty.e("Tile")
+			.tile(128, 128, "New Game", "img/plus.png")
+			.bind("Click", function ()
+			{
+				Crafty.scene("battle");
+			});
 	});
 
 	Crafty.background("#000");
 
 	// Title
-    Crafty.e("2D, DOM, Text").attr({ w: 100, h: 40, x: 150, y: 120 })
+    Crafty.e("2D, DOM, Text").attr({ w: 200, h: 40, x: 150, y: 120 })
             .text("Ad Astra:")
 			.textFont({ weight: "700", size: "40px", family: "Droid Sans"})
             .css({ "text-align": "center", "color": "white" });
--- js/tile.js
+++ js/tile.js
@@ -2,16 +2,53 @@ Crafty.c("Tile",
 {
 	init: function ()
 	{
-		this.requires("2D, DOM, Image, Text"); 
+		this.requires("2D, Mouse"); 
 	},
 
-	tile: function(text, image)
+	tile: function(w, h, text, imagePath)
 	{
-		this.text(text);
-		this.textFont({
-			size: "20px",
-			family: "Droid Sans"
-		});
-		this.css({ "color": "white" });
+		this.attr({ w: w, h: h });
+
+		var bb = Crafty.e("2D, DOM, Color")
+			.color("#007898");
+		bb.w = w;
+		bb.h = h;
+		this.attach(bb);
+
+		var b = Crafty.e("2D, DOM, Color")
+			.color("#009AC4");
+		b.x = 1;
+		b.y = 1;
+		b.w = w-2;
+		b.h = h-2;
+		this.attach(b);
+
+		if (text !== undefined)
+		{
+			var t = Crafty.e("2D, DOM, Text")
+				.text(text)
+				.textFont({
+					size: "20px",
+					family: "Droid Sans"
+				})
+				.css({ "color": "white", "text-align": "right" });
+			t.x = -5;
+			t.y = 5;
+			t.w = w;
+			t.z = 2;
+
+			this.attach(t);
+		}
+
+		if (imagePath !== undefined)
+		{
+			var i = Crafty.e("2D, DOM, Image")
+				.image(imagePath);
+			i.x = 10; 	
+			i.y = h - 10 - i.h;
+			i.z = 1;
+
+			this.attach(i);
+		}
 	}
 });
